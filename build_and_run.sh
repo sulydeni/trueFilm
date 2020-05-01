be10b54493549e81f3b2d542a1e071a4f6ce139b
EC2 Default User <sulydeni@gmail.com>
2020-05-01 02:40:55 +0000

    add sudo privilages for docker

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,18 +1,14 @@
-set +e
+set -e
 
 #install dependencies
 sudo yum install docker -y
 sudo service docker restart
-sudo groupadd docker || true
-sudo usermod -aG docker $USER
-
-newgrp docker < exit
 
 
 #run postgres docker
-docker pull postgres;
-mkdir -p /opt/docker/volumes/postgres;
-docker run --rm --name docker -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v /opt/docker/volumes/postgres:/var/lib/postgresql/data  postgres;
+sudo docker pull postgres;
+sudo mkdir -p /opt/docker/volumes/postgres;
+sudo docker run --rm --name docker -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v /opt/docker/volumes/postgres:/var/lib/postgresql/data  postgres;
 
 #download & unzip large work files
 #downlod the file if it does not exist
@@ -26,12 +22,12 @@ fi
 
 
 #build pyspark python environment and execution
-docker build . -t pyspark
+sudo docker build . -t pyspark
 
 #if test parameter exists run jupyter else execture the script
 if [[ $1 == "test" ]]
 then
-  docker run --network host -v `pwd`:/opt/code pyspark #run jupyter for testing
+  sudo docker run --network host -v `pwd`:/opt/code pyspark #run jupyter for testing
 else
-  docker run --network host -v `pwd`:/opt/code -w /opt/code pyspark /usr/bin/python3 main.py #execture the code
+  sudo docker run --network host -v `pwd`:/opt/code -w /opt/code pyspark /usr/bin/python3 main.py #execture the code
 fi
